0425a443d3e3e61362c4d7d7558ce976f2f76188
Roberto Rocha <robrs.web@gmail.com>
2020-06-26 16:50:41 -0300

    instalação do cors

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 const express = require('express')
-
+var cors = require('cors')
 const routes = require('./routes')
 
 require('./database')
 
 const app = express()
+app.use(cors())
 
 app.use(express.json())
 app.use(routes)
